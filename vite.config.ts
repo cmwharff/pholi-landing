9d5d90af50375ce6bfd051549fbb57b1f70fb764
cmwharff <115747231+cmwharff@users.noreply.github.com>
2025-12-15 13:09:38 -0500

    possible fix to base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import vue from "@vitejs/plugin-vue";
 import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'pholi-landing'
+// const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'pholi-landing'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
+  base: '/',
   css: {
     postcss: {
       plugins: [tailwind(), autoprefixer()],
